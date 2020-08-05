e678463e6fab036f85cb4ac494240a12125fcb83
omer332 <68290034+omer332@users.noreply.github.com>
2020-08-05 13:34:18 +0300

    Update Ride.java

--- Ride.java
+++ Ride.java
@@ -1,30 +1,16 @@
 
 public class Ride {
-	public enum station {
-		Hashlom("Tel Aviv-Hashalom"), HaAgana("Tel Aviv-HaHagana"), KfarSaba("Kfar Saba");
 
-		private String stationName;
-
-		private station(String station) {
-			this.stationName = station;
-		}
-
-		@Override
-		public String toString() {
-			return stationName;
-		}
-	}
-
-	private station origin;
-	private station destination;
+	private String origin;
+	private String destination;
 	private String leavingTime;
 	private String arrivalTime;
 	private int[] sort = new int[2];
-	
+
 	public static int idGenerator = 0;
 	private int rideNumber;
 
-	public Ride(station origin, station destination, String leavingTime, String arrivalTime) {
+	public Ride(String origin, String destination, String leavingTime, String arrivalTime) {
 		this.origin = origin;
 		this.destination = destination;
 		this.leavingTime = leavingTime;
@@ -33,11 +19,11 @@ public class Ride {
 		setSort(leavingTime);
 	}
 
-	public station getOrigin() {
+	public String getOrigin() {
 		return origin;
 	}
 
-	public station getDestination() {
+	public String getDestination() {
 		return destination;
 	}
 
@@ -52,29 +38,16 @@ public class Ride {
 	public int getrideNumber() {
 		return rideNumber;
 	}
-	
-	private int getSortHour() {
-		return sort[0];
-	}
-	private int getSortMin() {
-		return sort[1];
-	}
 
 	private void setSort(String leavingTime) {
 		String[] split = leavingTime.split(":");
-		sort[0] = Integer.parseInt(split[0]);
-		sort[1] = Integer.parseInt(split[1]);
+		sort[0] = Integer.parseInt(split[0]);// Hour
+		sort[1] = Integer.parseInt(split[1]);// Minute
 	}
 
-	public int compareToHours(Ride one, Ride two) {
-		return two.getSortHour()- one.getSortHour();
-	}
-	public int compareToMin(Ride one, Ride two) {
-		return two.getSortMin()- one.getSortMin();
-	}
 	@Override
 	public String toString() {
-		return origin.toString() + "," + leavingTime + "," + destination.toString() + "," + arrivalTime;
+		return origin + "," + leavingTime + "," + destination + "," + arrivalTime;
 	}
 
 }
