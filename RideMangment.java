1b73f6f5538f43c16d735059d9a00a505d296b79
lilach137 <69242062+lilach137@users.noreply.github.com>
2020-08-09 12:54:08 +0300

    Add files via upload

---
+++ RideMangment.java
@@ -0,0 +1,26 @@
+package sprint1;
+
+import java.util.ArrayList;
+
+public class RideMangment {
+
+	ArrayList<Ride> arrRide = new ArrayList<Ride>();
+
+
+	public void sortTime() {
+		for (int i = 0; i < arrRide.size()-1; i++) {
+			if (arrRide.get(i).getHours()>arrRide.get(i+1).getHours()) 
+				swap (arrRide.get(i), arrRide.get(i+1));
+			if (arrRide.get(i).getHours()==arrRide.get(i+1).getHours()) 
+				if (arrRide.get(i).getMinutes()>arrRide.get(i+1).getMinutes())
+					swap(arrRide.get(i), arrRide.get(i+1));
+		}
+	}
+
+	public void swap (Ride ride1, Ride ride2) {
+		Ride temp = ride1;
+		ride1 = ride2;
+		ride2 = temp;
+	}
+
+}
