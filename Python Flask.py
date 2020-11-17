04c620c8e399b1f6737266f6753ead1a8e310dbb
lilach137 <69242062+lilach137@users.noreply.github.com>
2020-11-17 06:26:52 +0200

    Update Python Flask.py

--- Python Flask.py
+++ Python Flask.py
@@ -7,11 +7,16 @@ app = Flask("my_app1")
 
 @app.route("/rail")
 def rail_schedule():
-    if 'outformat' in request.args:
-        outformat = request.args.get('outformat')
+    # for regular
+    if 'out_format' in request.args:
+        out_format = request.args.get('out_format')
+        # for web
     else:
-        outformat = "html"
-    return subprocess.check_output(["java", "-classpath", "bin", "WebData", outformat, request.args.get('origin'), request.args.get('destination'), request.args.get('hour'), request.args.get('minutes')])
+        out_format = "html"
+    return subprocess.check_output(["java", "-classpath", "bin", "WebData", out_format, request.args.get('origin'),
+                                    request.args.get('destination'), request.args.get('hour'),
+                                    request.args.get('minutes')])
 
 
 app.run(port=8005, host="0.0.0.0")
+# http://localhost:8005/rail?outformat=html&origin=tel_aviv&destination=beer_sheva&hour=23&minutes=30
